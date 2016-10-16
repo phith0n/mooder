15bfc9fbf372dd8334b4e7ea0b1ed896732efdc9
phith0n <phith0n.ph2f@gmail.com>
2016-10-16 23:09:58 +0800

    可以自定义db engine

--- mooder/settings_production.py
+++ mooder/settings_production.py
@@ -8,7 +8,7 @@ X_FRAME_OPTIONS = 'DENY'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': os.environ.get('DB_ENGINE', 'django.db.backends.postgresql') ,
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
