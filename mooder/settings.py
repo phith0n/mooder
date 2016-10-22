e88b03bfe074825cdb2a6d0ad8e7bdb8e1f2accc
phith0n <phith0n.ph2f@gmail.com>
2016-10-22 22:20:25 +0800

    默认DEBUG=True

--- mooder/settings.py
+++ mooder/settings.py
@@ -26,7 +26,7 @@ if os.path.exists(os.path.join(BASE_DIR, '.secretkey')):
         SECRET_KEY = f.read()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
